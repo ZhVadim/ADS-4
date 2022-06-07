878b77ebf1cf9979772c6f4cb4cb1d2ece638c6c
Вадим Желтов <99135277+ZhVadim@users.noreply.github.com>
2022-06-07 18:51:46 +0300

    ЖелтовИСТ3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
     int coup = 0;
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
                 if (arr[i] + arr[j] == value) {
                     coup += 1;
                 }
@@ -12,12 +12,9 @@ int countPairs1(int* arr, int len, int value) {
 }
 int countPairs2(int* arr, int len, int value) {
     int coup = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         if (arr[i] < 0) {
             i++;
-        }
-        if (arr[i] == value / 2) {
-            coup += 1;
         }
         if (arr[i] > value) {
             return coup;
@@ -37,7 +34,7 @@ int binSearch(int* arr, int len, int value, int kr) {
     int par = 0;
     int beg = kr, end = len - 1;
     while (beg < end - 1) {
-        int mid = (beg + (end + 1)) / 2;
+        int mid = (beg + end) / 2;
         if (arr[mid] == value) {
             par += 1;
             if (arr[mid + 1] == value) {
@@ -65,10 +62,7 @@ int countPairs3(int* arr, int len, int value) {
         if (arr[i] < 0) {
             i++;
         }
-        if (arr[i] == value / 2) {
-            coup += 1;
-        }
-        if (i > value) {
+        if (i >= value) {
             return coup;
         } else {
             if (arr[i] <= value / 2) {
